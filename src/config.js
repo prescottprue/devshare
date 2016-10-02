404c21bbed3b75156eea847ccede2888ced25de3
Scott Prue <scott@residenetwork.com>
2016-10-02 16:50:53 -0700

    Handling of shortened form of environment names added (dev, prod)

--- src/config.js
+++ src/config.js
@@ -9,13 +9,13 @@ export default {
     requests: 'requests'
   },
   firebase: {
-    prod: {
+    production: {
       apiKey: 'AIzaSyBQw-StxUsUhoQwZqaawegUFO7YOyG_HDA',
       authDomain: 'devshare-1.firebaseapp.com',
       databaseURL: 'https://devshare-1.firebaseio.com',
       storageBucket: 'devshare-1.appspot.com'
     },
-    dev: {
+    development: {
       apiKey: 'AIzaSyBuwR21cO0lMzMr_T-Dl_jG1dsORXZ1fwY',
       authDomain: 'devshare-stg.firebaseapp.com',
       databaseURL: 'https://devshare-stg.firebaseio.com',
